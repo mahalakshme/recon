28a5edece9328e78d3626463a4b894ec20b397f6
rdsubhas <rdsubhas@gmail.com>
2015-01-28 12:07:50 +0530

    Revert to puma

--- config/puma.rb
+++ config/puma.rb
@@ -1,8 +1,8 @@
-workers Integer(ENV['PUMA_WORKERS'] || 3)
-threads Integer(ENV['MIN_THREADS']  || 1), Integer(ENV['MAX_THREADS'] || 16)
+workers Integer(ENV['MIN_WORKERS'] || 2)
+threads Integer(ENV['MIN_THREADS']  || 1), Integer(ENV['MAX_THREADS'] || 8)
 
 rackup      DefaultRackup
-port        ENV['PORT'] || 8080
+port        ENV['PORT'] || 3000
 environment ENV['RAILS_ENV'] || 'production'
 preload_app!
 
