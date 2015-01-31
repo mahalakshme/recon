4e54599632ff9d7bcb4d67221f979ad8b79011d9
rdsubhas <rdsubhas@gmail.com>
2015-02-01 00:46:16 +0530

    Revert "Add date filter in Leaderboard"
    This reverts commit 2cb1d920d20d959de7ec356d7c88f51af1b6dba2.

--- app/admin/leaderboard.rb
+++ app/admin/leaderboard.rb
@@ -1,39 +1,44 @@
 ActiveAdmin.register Employee, as: 'Leaderboard', namespace: :guest do
 
+  JOIN_SQL = <<-QUERY
+    INNER JOIN (
+      SELECT e.id AS id,
+      COALESCE(SUM(s.points), 0) AS points
+      FROM employees e
+      LEFT OUTER JOIN employee_interviews ei ON ei.employee_id = e.id
+      LEFT OUTER JOIN interviews i ON (
+        i.id = ei.interview_id AND
+        i.interview_date >= '%{start_date}' AND
+        i.interview_date <= '%{end_date}'
+      )
+      LEFT OUTER JOIN stages s ON s.id = i.stage_id
+      GROUP BY e.id
+    ) points ON points.id = employees.id
+  QUERY
+
   menu priority: 1
   actions :index
 
   filter :role
   filter :grade
-  filter :interviews_interview_date, as: :date_range, label: 'Interview Date'
 
   config.sort_order = "points_desc"
 
   controller do
-    before_action do
-      params[:q] ||= {}
-      params[:q][:interviews_interview_date_gteq] = leaderboard_start_date
-      params[:q][:interviews_interview_date_lteq] = leaderboard_end_date
-    end
-
     def scoped_collection
-      Employee.includes_meta.joins(interviews: :stage).select('employees.*', 'coalesce(sum(stages.points), 0) points').group('employees.id')
+      Employee.includes_meta.joins(
+        JOIN_SQL % { start_date: leaderboard_date.at_beginning_of_month.iso8601, end_date: leaderboard_date.at_end_of_month.iso8601 }
+      ).select(
+        "employees.id, employees.employee_ref, employees.name, employees.role_id, employees.grade_id, points.points"
+      )
     end
 
-    def leaderboard_start_date
-      @start_date ||= Time.zone.parse(params[:q][:interviews_interview_date_gteq]) rescue Time.zone.now.at_beginning_of_month
-    end
-
-    def leaderboard_end_date
-      @end_date ||= Time.zone.parse(params[:q][:interviews_interview_date_lteq] + ' 23:59:59') rescue Time.zone.now.at_end_of_month
-    end
-
-    def leaderboard_title
-      'Leaderboard for %s - %s' % [leaderboard_start_date.strftime('%b %d'), leaderboard_end_date.strftime('%b %d')]
+    def leaderboard_date
+      @leaderboard_date ||= (Time.zone.parse(params[:date]) rescue Time.zone.now)
     end
   end
 
-  index download_links: false, title: Proc.new{ leaderboard_title } do
+  index title: Proc.new{ "Leaderboard for #{leaderboard_date.strftime('%b %Y')}" }, download_links: false do
     column :employee_ref
     column :name do |e|
       link_to e.name, [:admin, e]
@@ -48,8 +53,8 @@ ActiveAdmin.register Employee, as: 'Leaderboard', namespace: :guest do
   end
 
   collection_action :contribution_report, method: :get do
-    csv = ContributionReport.generate Employee.includes_meta, leaderboard_start_date, leaderboard_end_date
-    filename = "contribution_report.csv"
+    csv = ContributionReport.generate(Employee.includes_meta, leaderboard_date.at_beginning_of_month, leaderboard_date.at_end_of_month)
+    filename = "contribution_report_#{leaderboard_date.strftime('%b_%Y')}.csv"
     send_data csv, filename: filename, type: 'text/csv'
   end
 
